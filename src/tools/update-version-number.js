531cf9ac741a7424b8766b350db46155a87239bd
Matthias Reis <matthias.reis@xing.com>
2017-03-08 18:44:10 +0100

    chore: removed console.log

--- src/tools/update-version-number.js
+++ src/tools/update-version-number.js
@@ -5,7 +5,6 @@ module.exports = (nsp, packages) => pkg => {
   const incrementLevelThroughRelation = pkg[nsp].relatedMessages.length > 0 ? 0 : -1;
   const incrementLevelsThroughMessages = pkg[nsp].messages.map(msg => msg.level);
 
-  console.log(pkg[nsp].relatedMessages);
   pkg[nsp].determinedIncrementLevel = R.last([incrementLevelThroughRelation, ...incrementLevelsThroughMessages].sort());
 
   pkg.version = bump(pkg.version, pkg[nsp].determinedIncrementLevel);
