8e042b6d562c8d93ef6f0c849dbb74f67bcfad94
Matthias Reis <matthias.reis@xing.com>
2017-03-08 11:31:42 +0100

    feat(relations): removed support for recursive relations

--- src/tools/assign-related-messages-to-package.js
+++ src/tools/assign-related-messages-to-package.js
@@ -1,7 +1,8 @@
 module.exports = (nsp, pkgs) => pkg => {
   const informRelated = (msg, relatedPackageName) => {
     pkgs[relatedPackageName][nsp].relatedMessages.push(msg);
-    pkgs[relatedPackageName][nsp].relations.forEach(pkgName => informRelated(msg, pkgName));
+    // no recursion - only direct relations are bumped
+    // pkgs[relatedPackageName][nsp].relations.forEach(pkgName => informRelated(msg, pkgName));
   };
 
   pkg[nsp].relations.forEach(pkgName => pkg[nsp].messages.forEach(msg => informRelated(msg, pkgName)));
