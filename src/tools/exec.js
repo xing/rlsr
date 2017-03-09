bfe7e46292840a86883563b7c3a0056890a9e269
Matthias Reis <matthias.reis@xing.com>
2017-03-09 11:10:28 +0100

    chore(exec): logging of dir

--- src/tools/exec.js
+++ src/tools/exec.js
@@ -3,14 +3,14 @@ const cp = require('child_process');
 
 module.exports = R.curry((log, dbg, cwd, line) => new Promise(
   (resolve, reject) => {
-    console.log(line, cwd);
     const t = Date.now();
     setTimeout(() => {
-      log(`executing <${line}>`);
+      const dir = cwd && 'current dir';
+      log(`executing <${line}> in <${dir}`);
       const opts = cwd ? {cwd} : null;
       try {
         cp.exec(line, opts, (error, stdout, stderr) => {
-          log(`<${line}> done in ${Date.now() - t}ms`);
+          log(`<${line}> in <${dir} done in ${Date.now() - t}ms`);
           dbg(stdout);
           dbg(stderr);
           if (error) {
