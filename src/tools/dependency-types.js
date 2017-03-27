2553c17a9bce187df488e392d57a5a22780b1feb
Hannes Diercks <hannes.diercks@xing.com>
2017-03-27 13:32:07 +0200

    refactor: involve dev and peer dependencies when polishing package

---
+++ src/tools/dependency-types.js
@@ -0,0 +1,5 @@
+
+exports.KEY_DEPENDENCIES = 'dependencies';
+exports.KEY_PEER_DEPENDENCIES = 'peerDependencies';
+exports.KEY_DEV_DEPENDENCIES = 'devDependencies';
+exports.dependencyKeys = [exports.KEY_DEPENDENCIES, exports.KEY_PEER_DEPENDENCIES, exports.KEY_DEV_DEPENDENCIES];
