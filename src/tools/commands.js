cd05cdf20c8f4dca2f2c5d927660a3a9e545db76
Matthias Reis <matthias.reis@xing.com>
2017-05-15 17:43:54 +0200

    fix(perform): Only files touched by the process are committed
    closes #23

--- src/tools/commands.js
+++ src/tools/commands.js
@@ -1,5 +1,6 @@
-const exec = require('./exec');
+const path = require('path');
 const R = require('ramda');
+const exec = require('./exec');
 
 const command = R.curry((log, dbg, dir, cmd, description) => {
   log(`exec command: ${description}`);
@@ -9,11 +10,18 @@ const command = R.curry((log, dbg, dir, cmd, description) => {
 module.exports = (log, dbg) => {
   const runInDir = command(log, dbg);
   const run = runInDir(null);
+
   return {
-    commitChanges: version => run(
-      `git add . && git commit -m "chore: release ${version}"`,
-      `committing changelogs for version <${version}>`
-    ),
+    commitChanges: (version, packageAbsPath, packages) => {
+      const files = R.flatten(packages.map(p => [
+        path.join(packageAbsPath, p, 'changelog.md'),
+        packages.map(p => [path.join(packageAbsPath, p, 'package.json')])
+      ])).join(' ');
+      return run(
+        `git add ${files} && git commit -m "chore: release ${version}"`,
+        `committing changelogs for version <${version}>`
+      );
+    },
     tagPackage: (name, version) => run(
       `git tag -a -m 'chore: tagged ${name}@${version}' ${name}@${version}`,
       `adding git tag for <${name}@${version}>`
@@ -24,7 +32,7 @@ module.exports = (log, dbg) => {
       `publishing <${name}@${version}>`
     ),
     commitMain: version => run(
-      `git add . && git commit -m "chore: update main package ${version}"`,
+      `git add package.json && git commit -m "chore: update main package ${version}"`,
       `committing changes for <${version}>`
     ),
     tagMain: version => run(
