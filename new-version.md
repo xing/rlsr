1d8a0e534d502feea80e8fd2cfc04fc6f7b9c3c1
github-script <github-script@gnix.com>
2021-05-31 18:07:11 +0200

    docs: documentin new version features

--- new-version.md
+++ new-version.md
@@ -1,7 +1,41 @@
 # Version 4.0
 
-- move to typescript
-- add subfolder support - each subfolder is an rgroup
-- rgroups are being released with the same version number
-- add support for creating an additional file with all current version numbers
-- have one file in root that persists all version numbers
+## move to typescript
+
+Code will be ported to typescript and rearranged a little bit.
+
+## Versionfile
+
+We introduce a `rlsr.json` that contains all stored version and dependency data.
+This file will be committed to the repo. It also contains the commit hash, the
+scrip has been worked on last.
+
+E.g.
+
+```json
+{
+  "@xingternal/button": {
+    "version": "30.1.1",
+    "dependencies": {
+      "@xingternal/typography": "20 - 23",
+      "@xingternal/icons": "29 - 40"
+    }
+  }
+}
+```
+
+In the actual package.json files, we replace the versions with simple
+placeholders`"@xingternal/button": "*"`. That makes it much easier to do a
+release in multi owner repos because these files remain very constant and will
+never be touched by the release process.
+
+## Changelog
+
+In previous versions, the changelog has been persisted to static md files. This
+time, we wil persist the raw data in JSOn files so they are usable in the
+brewery app.
+
+## Independent Mode
+
+for the sake of simplifying, we only support independent mode from now on.
+Meaning all packages are versioned independently from each other.
--- new/analyse/index.ts
+++ new/analyse/index.ts
@@ -4,14 +4,41 @@ import { log } from '../helpers/log-module';
 
 export const analyse = composeAsync(
   log('ANALYSE PHASE: Looking at what needs to be changed'),
+
+  // See polish-packages.js
+  // prefill package data with empty defaults
+  // and add all dependencies (default, dev and peer)
+  // addDependencies
+
+  // See refine-messages.js
+  // add metadata to the messages by parsing
+  // refineMessages
+
+  // See add-relations-to-packages.js
   // this creates a tree of dependencies - directly connects the files to each other
   // and also adds the inverted relation (parent-child/child-parent)
   // createDependencyTree,
 
+  // see add-messages-to-packages.js
+  // a package now contains a list of messages with severities etc.
+  // addMessagesToPackages
+
+  // see calculate-overall-increment.js
+  // for information purposes, we create an overall version number
+  // calculateRepoVersion
+
+  // the next four should possibly created from scratch - it's the heart of the system
+  // references are
+  // - src/transform/synchronized-add-related-messages.js
+  // - src/transform/synchronized-update-all-versions.js
+  // - src/transform/unsynchronized-calculate-new-version.js
+  // - src/transform/unsynchronized-update-relations.js
+
   // based on the currently changed files that come from relevant commits
-  // (major, minor, patch level) find all packages that are affected, too.
+  // (major, minor, patch level) find all packages that are indirectly affected, too.
   // e.g. if button is updated, all packages that depend on button are affected
   // as well and are put on a list.
+  // Please take into account that the packages might be new.
   // findAffectedPackages,
 
   // Find out how much the package must be incremented
@@ -24,5 +51,15 @@ export const analyse = composeAsync(
   // knowing the new version number, the dependency from findAffectedPackages
   // can be changes as well.
   // changeDependencies,
+
+  // see calculate-main-changelog-entries.js
+  // Changelog must be prepared correctly, so the relevant data must be arranged
+  // for each package
+  // prepareChangelog
+
+  // there surely is stuff to clanup and rearrange
+  // this is the place for it - before parts of the env are being written down
+  // cleanUp
+
   wait(1000)
 );
