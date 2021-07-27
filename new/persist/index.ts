13d82534c75a58f112f7477266af3a48e76559d2
Santiago Petrone <santiago.petrone@xing.com>
2021-07-27 16:51:17 +0200

    chore: adapt pending tasks

--- new/persist/index.ts
+++ new/persist/index.ts
@@ -17,7 +17,7 @@ export const persist = composeAsync(
   // after the package is publishet, we bring back the package.jsons with the `*`
   // dependencies. The data structure should be in the env.
   // This is a very similar step to change > writeToPackageJsons
-  // revertPackageJsons
+  // writePackageJsonsForGit
 
   // finally the changed files must be added to git.
   // it's a
@@ -25,6 +25,8 @@ export const persist = composeAsync(
   // - `git commit -m "<a useful commit message>"`
   //    should probably contain all the bumps in the message body?!?
   // - git tag for each changed package (tag format `@xingternal/button@32.1.1`)
+  // commitAndTagPackages
+
   // - `git pull`
   // - `git push`
   // - `git push --tags`
@@ -33,7 +35,7 @@ export const persist = composeAsync(
   // But I don't expect larger tree conflicts to happen anymore.
   // This commit is suppoesd to contain only rlsr.json and changelogs
   // package.jsons should only be in there if new packages are being added.
-  // commitAndPushChanges
+  // pushChanges
 
   wait(1000)
 );
