6d9a9f1569adc0ac71cb60d5b41d9b5e108a3711
github-script <github-script@gnix.com>
2021-07-30 17:20:38 +0200

    fix: types to levels

--- src/collect/parse-commit-messages/add-level.ts
+++ src/collect/parse-commit-messages/add-level.ts
@@ -13,7 +13,9 @@ export const addLevel = (
         message.level = 'minor';
         break;
       case 'fix':
-      case 'style':
+      case 'refactor':
+      case 'perf':
+      case 'revert':
         message.level = 'patch';
         break;
       default:
